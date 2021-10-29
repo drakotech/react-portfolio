8133edb7eebbf61d5e18c28b01dd8e509ca70edd
drakotech <george.flefel@gmail.com>
2021-10-28 18:26:00 -0700

    Started Styling Homepage

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import moment from "moment";
 import {
   BrowserRouter as Router,
   Switch, 
@@ -17,11 +16,9 @@ import NoMatch from "./pages/no-match"
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className='container'>
         <Router>
           <div>
-            <h1>George Flefel Portfolio</h1>
-            <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
             <NavigationContainer />
             
             <Switch>
